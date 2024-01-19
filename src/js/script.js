97ff4cba6adbaf9686661eb05841be07f2daede3
Yuriy <xr007@outlook.com>
2024-01-19 13:42:34 +0200

    test project

---
+++ src/js/script.js
@@ -0,0 +1,54 @@
+$('ul.test__tabs').on('click', 'li:not(.test__tab_active)', function () {
+    $(this)
+        .addClass('test__tab_active').siblings().removeClass('test__tab_active')
+});
+
+window.onload = function () {
+    var paginationPage = parseInt($('.cdp').attr('actpage'), 10);
+    $('.cdp_i').on('click', function () {
+        var go = $(this).attr('href').replace('#!', '');
+        if (go === '+1') {
+            paginationPage++;
+        } else if (go === '-1') {
+            paginationPage--;
+        } else {
+            paginationPage = parseInt(go, 10);
+        }
+        $('.cdp').attr('actpage', paginationPage);
+    });
+};
+
+$(document).ready(function () {
+    $('.carousel').slick({
+        dots: true,
+        infinite: false,
+        arrows: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 766,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 375,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
+
